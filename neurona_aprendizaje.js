d3049fdc4965a4ea19bca8863f145f5ffd24ea18
Martin Sarabia <martin@brightcoders.com>
2017-07-03 16:25:54 -0500

    Refactorizando Neurona

--- neurona_aprendizaje.js
+++ neurona_aprendizaje.js
@@ -3,6 +3,51 @@
  */
 "use strict";
 
+class neurona {
+  constructor() {
+    this.weight = {};
+    this.inputs = {};
+
+    //umbral de activacion
+    this.U;
+    this.outputs = {};
+
+    //Factor de aprendizaje
+    this.E = 0.6;
+
+  }
+
+  learning(training, E, U) {
+    var i    = 0;
+    var cont = 1;
+
+    while (i < training.length && cont < 1000) {
+      y = Math.tanh((training[i][0] * w1 ) + (training[i][1] * w2 ) + (-1 * θ));
+      y = (y >= θ) ? 1 : -1;
+      console.log("Entrada:", training[i][0], training[i][1], "Valor Esperado:", training[i][2], "Salida:", y);
+
+      if (y == training[i][2]) {
+        i++;
+      }
+      else {
+        // Se ajustan los pesos
+        w1 = w1 + 2 * E * training[i][2] * training[i][0];
+        w2 = w2 + 2 * E * training[i][2] * training[i][1];
+        θ  = θ + 2 * E * training[i][2] * (-1);
+
+        console.log("Ajustando pesos #", cont);
+        console.log("w1:", w1, "W2:", w2, "θ:", θ);
+        cont++;
+        i = 0;
+      }
+    }
+
+  }
+
+  resolve() {
+  }
+}
+
 var tabla_verdad = [
   [1, 1, 1],
   [1, -1, 1],
@@ -29,7 +74,7 @@ var cont = 1;
 
 while (i < tabla_verdad.length && cont < 1000) {
   y = Math.tanh((tabla_verdad[i][0] * w1 ) + (tabla_verdad[i][1] * w2 ) + (-1 * θ));
-  y = (y >= θ)  ? 1 : -1;
+  y = (y >= θ) ? 1 : -1;
   console.log("Entrada:", tabla_verdad[i][0], tabla_verdad[i][1], "Valor Esperado:", tabla_verdad[i][2], "Salida:", y);
 
   if (y == tabla_verdad[i][2]) {
@@ -56,19 +101,19 @@ if (cont <= 9999) {
 
   y = ((-1 * w1) + (-1 * w2) + (-1 * θ));
   y = (y > θ) ? 1 : -1;
-  console.log(-1,-1, y);
+  console.log(-1, -1, y);
 
   y = ((-1 * w1) + (1 * w2) + (-1 * θ));
   y = (y > θ) ? 1 : -1;
-  console.log(-1,1, y);
+  console.log(-1, 1, y);
 
   y = ((1 * w1) + (-1 * w2) + (-1 * θ));
   y = (y > θ) ? 1 : -1;
-  console.log(1,-1, y);
+  console.log(1, -1, y);
 
   y = ((1 * w1) + (1 * w2) + (-1 * θ));
   y = (y > θ) ? 1 : -1;
-  console.log(1,1, y);
+  console.log(1, 1, y);
 }
 
 // Ejecucion
